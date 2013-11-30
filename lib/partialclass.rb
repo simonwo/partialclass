bb2c1115abe13ba60bab5d17b52110c48d202331
Simon Worthington <simon@simonwo.net>
2013-11-30 23:14:33 +0000

    Use `new` instead of `initialize` to better handle core classes. - Many built-in classes don't use an `initialize` method, but most   of them do support `new`.

--- lib/partialclass.rb
+++ lib/partialclass.rb
@@ -12,15 +12,15 @@ class Class
 	unless Class.respond_to? :define_singleton_method
 		def define_singleton_method name, &block
 			metaclass = class << self; self; end
-			metaclass.define_method(:name, &block)
+			metaclass.define_method(name, &block)
 		end
 	end
 
 	private
 	def _specialize(parent, *prep)
 		Class.new(parent) do
-			define_method(:initialize)           { |*args| super(*(prep + args)) }
-			define_singleton_method(:specialize) { |*args|  _specialize(parent, *(prep+args)) }
+			define_singleton_method(:new) { |*args| super(*(prep + args)) }
+			define_singleton_method(:specialize) { |*args| _specialize(parent, *(prep+args)) }
 		end
 	end
 end
